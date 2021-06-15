0e37c5f3463503d3c62c87c1f1642e901d4644c9
nulay <nulay@mail.ru>
2021-06-16 00:19:50 +0300

    заставил камеру записывать видео

---
+++ app/src/main/java/com/example/homebotrecorder/ui/video/CameraAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.homebotrecorder.ui.video;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+public class CameraAppActivity extends AppCompatActivity {
+   
+}
